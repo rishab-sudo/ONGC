6af86b1c724ea61e7c8408ee396b1260668862c1
unknown <rishabdutt4@gmail.com>
2025-11-01 11:24:10 +0530

    30-10-25

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,16 +6,16 @@ const Header = () => {
     <div className="scrolling-header">
       <div className="scrolling-track">
         <div className="scrolling-text">
-          (CBSE Affiliation No. 2132671) | Email: info@ongcschool.com | Phone: +91 8009244441
+          CBSE Affiliation No. 2132671 | Email: info@ongcschool.com | Phone: +91 8009244441
         </div>
         <div className="scrolling-text">
-          (CBSE Affiliation No. 2132671) | Email: info@ongcschool.com | Phone: +91 8009244441
+          CBSE Affiliation No. 2132671 | Email: info@ongcschool.com | Phone: +91 8009244441
         </div>
         <div className="scrolling-text">
-          (CBSE Affiliation No. 2132671) | Email: info@ongcschool.com | Phone: +91 8009244441
+          CBSE Affiliation No. 2132671 | Email: info@ongcschool.com | Phone: +91 8009244441
         </div>
         <div className="scrolling-text">
-          (CBSE Affiliation No. 2132671) | Email: info@ongcschool.com | Phone: +91 8009244441
+          CBSE Affiliation No. 2132671 | Email: info@ongcschool.com | Phone: +91 8009244441
         </div>
       </div>
     </div>
