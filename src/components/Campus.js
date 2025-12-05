d15ee666c2404dda16e706ed945ccfdbe0e23f4b
unknown <rishabdutt4@gmail.com>
2025-12-05 16:55:18 +0530

    5122024

--- src/components/Campus.js
+++ src/components/Campus.js
@@ -39,7 +39,7 @@ const CampusSection = () => {
     arrows: false,
     responsive: [
       { breakpoint: 1024, settings: { slidesToShow: 3 } },
-      { breakpoint: 768, settings: { slidesToShow: 2 } },
+      { breakpoint: 768, settings: { slidesToShow: 1 } },
       { breakpoint: 576, settings: { slidesToShow: 1 } },
     ],
   };
