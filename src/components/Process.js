5edad99715e47f2f13dad8605135c64738dc4488
unknown <rishabdutt4@gmail.com>
2025-12-08 15:06:02 +0530

    8122024

--- src/components/Process.js
+++ src/components/Process.js
@@ -16,21 +16,16 @@ const Process = () => {
         <div className="overlay"></div>
 
         <Container className="center-content">
-          <h1>Who We Are</h1>
+          <h1 className="page-heading process-heading">Who We Are</h1>
 
-          <p>
-            ONGC Community School is a progressive educational institution committed 
-            to providing quality education, holistic development, and value-based learning.
+          <p className="page-text">
+ONGC Community School is a progressive educational
+ institution committed to providing quality education, holistic development, and value-based learning. We strive to empower every child with academic excellence, life skills, discipline, and confidence to excel in a dynamic world.
           </p>
-          <p>
-            We strive to empower every child with academic excellence, life skills, 
-            discipline, and confidence to excel in a dynamic world.
-          </p>
-
+       
           {showMore1 && (
-            <p className="more-text">
-              It identifies talent at a young age and transforms it into future-ready 
-              leaders through holistic learning, exposure and mentorship.
+            <p className="more-text page-text">
+             
             </p>
           )}
 
@@ -50,17 +45,16 @@ const Process = () => {
         <div className="overlay"></div>
 
         <Container className="center-content">
-          <h1>What We Do</h1>
+          <h1  className="page-heading process-heading">What We Do</h1>
 
-          <p>
-            VidyaGyan offers a world-class residential model that nurtures excellence
-            through academic rigour, exposure-based learning and character development.
+          <p className="page-text">
+           We empower students through a blend of academics, co-curricular activities, sports, and value-based learning.
+            Our mission is to nurture each child’s potential, build strong character, and prepare them for future success in a rapidly changing world.
           </p>
 
           {showMore2 && (
-            <p className="more-text">
-              Students are empowered with leadership skills, innovation mindset,
-              emotional intelligence & real-world learning.
+            <p className="more-text page-text">
+        
             </p>
           )}
 
